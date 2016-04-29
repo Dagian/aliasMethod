a95368a75b46cc79575cd8ed87f6bd3341b8cb9a
Dagian <fjprevi@gmail.com>
2016-04-29 12:30:32 -0300

    v1.0

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <utility>
+#include <iostream>
+#include "aliasMethod.h"
+using namespace std;
+int main(){      
+    vector<pair<int, double>> dist;
+    dist.push_back(make_pair(0, 0.2));
+    dist.push_back(make_pair(1, 0.15));
+    dist.push_back(make_pair(2, 0.05));
+    dist.push_back(make_pair(3, 0.05));
+    dist.push_back(make_pair(4, 0.15));
+    dist.push_back(make_pair(5, 0.2));
+    dist.push_back(make_pair(6, 0.05));
+    dist.push_back(make_pair(7, 0.05));
+    dist.push_back(make_pair(8, 0.06));
+    dist.push_back(make_pair(9, 0.04));
+    discreteProbabilityDistribution <int> aliasM(dist);
+    vector<int> appearances(10, 0);
+    for(int i = 0; i < 1000; ++i){
+        int rolling = aliasM.roll();
+        appearances[rolling]++;
+    }
+    for(int i = 0; i < 10; ++i){
+        cout << i << ": " << appearances[i] << endl;
+    }
+    return 0;
+}
